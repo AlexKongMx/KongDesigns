c35416c87b0dccff174785fa7edeaa5ef662cb86
Alex Kong <116298126+AlexKongMx@users.noreply.github.com>
2026-08-21 01:30:45 -0500

    Preview fix full-width hero and ticker containment

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./site/App";
 import "./site/styles.css";
 import "./experience/experience.css";
+import "./site/hero-fix.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
