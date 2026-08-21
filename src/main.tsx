bc26497d42b425392c2c167d360b7a25729af260
Alex Kong <116298126+AlexKongMx@users.noreply.github.com>
2026-08-21 10:49:09 -0700

    Route hidden /precios configurator

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./site/App";
+import PricingPage from "./pricing/PricingPage";
 import "./site/styles.css";
 import "./experience/experience.css";
 import "./site/hero-fix.css";
+import "./pricing/pricing.css";
+
+const isPricingPage = window.location.pathname.replace(/\/+$/,"") === "/precios";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    {isPricingPage ? <PricingPage /> : <App />}
   </StrictMode>,
 );
